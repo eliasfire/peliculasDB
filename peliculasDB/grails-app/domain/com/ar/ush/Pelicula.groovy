988d81b6d3819ad2fc673b259f955ad904be6f97
avraham <avraham@info-07>
2016-02-10 16:16:43 -0300

    horarios

--- peliculasDB/grails-app/domain/com/ar/ush/Pelicula.groovy
+++ peliculasDB/grails-app/domain/com/ar/ush/Pelicula.groovy
@@ -2,6 +2,13 @@ package com.ar.ush
 
 class Pelicula {
 
+	String titulo
+	String sinopsis
+	Persona director
+	Integer duracion
+	
     static constraints = {
-    }
+		        sinopsis maxSize: 4000
+				titulo nullable: true
+	}
 }
